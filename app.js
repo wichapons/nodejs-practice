4e6b5359344814c7633ca97cd6c1f1157880921a
Wichapon V <wichapon@gmail.com>
2023-02-15 17:50:27 +0700

    implement ejs for seperating duplicate format like header footer and content to includes folder

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
 const express = require('express');
-const path = require('path')
-
+const path = require('path');
+const fs = require('fs');
 const app = express();
 
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs'); // setting the view engine to ejs
+app.use(express.urlencoded({extended:false}));
+app.use(express.static('public')); //make html can access static file like css and js
+
 app.get('/',(req,res)=>{
-    const FilePath = path.join(__dirname,'views','index.html')
-       res.sendFile(FilePath)
+    res.render('index');
     });
 
 app.get('/restaurants',(req,res)=>{
-    let FilePath = path.join(__dirname,'views','restaurants.html')
-   res.sendFile(FilePath)
+    const shopDataPath = path.join(__dirname,'data','restaurants.json');
+    const fileData = fs.readFileSync(shopDataPath);
+    const storedShop = JSON.parse(fileData);
+
+    res.render('restaurants',{numerOfShop:storedShop.length,restaurants:storedShop});
 });
 
 app.get('/recommend',(req,res)=>{
-    let FilePath = path.join(__dirname,'views','recommend.html')
-       res.sendFile(FilePath)
+    res.render('recommend');
     });
+
+app.post('/recommend',(req,res)=>{
+    const shopName = req.body; //all input details after user submit the form
+    console.log(shopName);
+    const shopDataPath = path.join(__dirname,'data','restaurants.json');
+    const fileData = fs.readFileSync(shopDataPath);
+    const storedShop = JSON.parse(fileData);
+    storedShop.push(shopName);
+    fs.writeFileSync(shopDataPath,JSON.stringify(storedShop))
+    res.redirect('/confirm');
+
+    /*
+    for (const shopList of storedShop){
+        const displayShopList = '<ul>';
+        displayShopList += '<li>' + shopList + '</li>';
+        
+    }
+    res.send(shopList)
+    */
+
+})
+
 app.get('/about',(req,res)=>{
-    let FilePath = path.join(__dirname,'views','about.html')
-       res.sendFile(FilePath)
+    res.render('about');
     });
 app.get('/confirm',(req,res)=>{
-    let FilePath = path.join(__dirname,'views','confirm.html')
-           res.sendFile(FilePath)
-        });
-
-
-
-
+    res.render('confirm');
+    });
 
 
 
