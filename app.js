638e00088bd305e95281ac2021a3809bc2e3ad26
Wichapon V <wichapon@gmail.com>
2023-02-16 11:04:16 +0700

    implement ejs to create dynamic website

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid')
+
+
 const app = express();
 
 app.set('views',path.join(__dirname,'views'));
@@ -20,12 +23,27 @@ app.get('/restaurants',(req,res)=>{
     res.render('restaurants',{numerOfShop:storedShop.length,restaurants:storedShop});
 });
 
+app.get('/restaurants/:id',(req,res)=>{
+    const shopID = req.params.id;
+    const shopDataPath = path.join(__dirname,'data','restaurants.json');
+    const fileData = fs.readFileSync(shopDataPath);
+    const storedShop = JSON.parse(fileData);
+
+    for (const shop of storedShop){
+        if(shop.id === shopID){
+           return res.render('restaurants-details',{id:shop.name}); //use return for tell program to stop looping for more cuz we already found the result
+        }else{
+            console.log("check shop.id");
+        }} 
+});
+
 app.get('/recommend',(req,res)=>{
     res.render('recommend');
     });
 
 app.post('/recommend',(req,res)=>{
-    const shopName = req.body; //all input details after user submit the form
+    const shopName = req.body; //all input details after user submit the form as an object
+    shopName.id = uuid.v4();  // shopName.id // in javascipt if we access to the new property that's not in the object yet JS will create for you unlike the other programming lang
     console.log(shopName);
     const shopDataPath = path.join(__dirname,'data','restaurants.json');
     const fileData = fs.readFileSync(shopDataPath);
